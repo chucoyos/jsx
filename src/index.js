1d80039e57a6fa5534d745c0bdb0eb68af875152
chucoyos <facturasmanzanillo@hotmail.com>
2021-09-14 00:56:39 -0500

    add component, variables and objects

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function getButtonText() {
+    return 'Click on Me';
+}
+
+const App = function () {
+    const labelText = 'Enter Name:'
+    const paragraphText = { text: 'This text'}
+    return (
+        <div>
+            <p>{paragraphText.text}</p>
+            <label className="label" htmlFor="name">{labelText} </label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>
+              {getButtonText()}
+            </button>
+        </div>
+    );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ <App />,
+ document.querySelector('#root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
